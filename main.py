70088fba4e2306abd2f00ce58e63256bd62f0118
Parthipan-Natkunam <pnatkunam@babbel.com>
2024-07-15 21:57:05 +0200

    feat: create a basic RAG chatbot

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
-from ai import getLLM
+from llama_index.core import (
+    Settings
+)
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.chat_engine import ContextChatEngine
+from ai import get_llm
+from embedder import get_embed_model, create_vector_embeddings
 
-llm = getLLM()
 
-print(llm.complete("What is Pluto?"))
+
+def setup_llama_index():
+    Settings.llm = get_llm()
+    Settings.chunk_size = 512
+    Settings.chunk_overlap = 64
+    Settings.embed_model = get_embed_model()    
+
+def main():
+    setup_llama_index()
+    vector_store = create_vector_embeddings()
+    retriever = VectorIndexRetriever(vector_store)
+    query_engine = RetrieverQueryEngine.from_args(
+        retriever=retriever
+    )
+
+    chat_engine = ContextChatEngine.from_defaults(
+        retriever=retriever,
+        query_engine=query_engine,
+        verbose=True
+    )
+
+    chat_engine.chat_repl()
+
+
+
+if __name__ == "__main__":
+    main()
